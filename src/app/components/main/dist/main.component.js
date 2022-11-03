d9d966b26da4470f4063223ae6d0769f83e050f2
Dmytro Syvenko <syvenkod@gmail.com>
2022-11-03 18:39:42 +0200

    some animation

--- src/app/components/main/dist/main.component.js
+++ src/app/components/main/dist/main.component.js
@@ -8,8 +8,10 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 exports.__esModule = true;
 exports.MainComponent = void 0;
 var core_1 = require("@angular/core");
+var animations_1 = require("@angular/animations");
 var MainComponent = /** @class */ (function () {
     function MainComponent() {
+        this.show = true;
         // Carousel
         this.customOptions = {
             autoplay: true,
@@ -20,28 +22,46 @@ var MainComponent = /** @class */ (function () {
             pullDrag: false,
             dots: true,
             navSpeed: 2000,
-            navText: ['<', '>',],
+            navText: ['', '',],
             responsive: {
                 1024: {
                     items: 1
                 }
             },
-            nav: true
+            nav: false
         };
         this.slides = [
-            { id: '1', img: "assets/slides/slide-01.jpg" },
-            { id: '2', img: "assets/slides/slide-02.jpg" },
-            { id: '3', img: "assets/slides/slide-03.jpg" },
-            { id: '4', img: "assets/slides/slide-04.jpg" },
+            { id: '1', img: "assets/slides/slide-01.jpg", title: "Women Collection 2018", desc: "NEW SEASON", btn: "Shop Now" },
+            { id: '2', img: "assets/slides/slide-02.jpg", title: "Men New-Season", desc: "Jackets & Coats", btn: "Shop Now" },
+            { id: '3', img: "assets/slides/slide-03.jpg", title: "Men Collection 2018", desc: "New arrivals", btn: "Shop Now" },
+            { id: '4', img: "assets/slides/slide-04.jpg", title: "Women Collection 2018", desc: "New arrivals", btn: "Shop Now" },
         ];
     }
     MainComponent.prototype.ngOnInit = function () {
+        // setInterval(()=>this.show = !this.show, 8000)
+    };
+    MainComponent.prototype.ngAfterContentChecked = function () {
+        this.show = !this.show;
     };
     MainComponent = __decorate([
         core_1.Component({
             selector: 'app-main',
             templateUrl: './main.component.html',
-            styleUrls: ['./main.component.scss']
+            styleUrls: ['./main.component.scss'],
+            animations: [
+                animations_1.trigger('query', [
+                    animations_1.transition(':enter', [
+                        animations_1.style({ height: 0 }),
+                        animations_1.group([
+                            animations_1.animate(500, animations_1.style({ height: '*' })),
+                            animations_1.query(':enter', [
+                                animations_1.style({ opacity: 0, transform: 'scale(0)' }),
+                                animations_1.animate(2000, animations_1.style({ opacity: 1, transform: 'scale(1)' }))
+                            ]),
+                        ]),
+                    ]),
+                ]),
+            ]
         })
     ], MainComponent);
     return MainComponent;
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,16 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, Component, OnInit } from '@angular/core';
 import { OwlOptions } from 'ngx-owl-carousel-o';
+import {
+  trigger,
+  style,
+  animate,
+  transition,
+  group,
+  query,
+  animateChild,
+  keyframes,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
+  animations: [
+    trigger('query', [
+      transition(':enter', [
+        style({ height: 0 }),
+        group([
+          animate(500, style({ height: '*' })),
+          query(':enter', [
+            style({ opacity: 0, transform: 'scale(0)'}),
+            animate(2000, style({ opacity: 1, transform: 'scale(1)' }))
+          ]),
+          // query('.btn', [
+          //   style({ transform: 'translateX(-100%)'}),
+          //   animate('.7s 500ms ease-in', style({ transform: 'translateX(0)' }))
+          // ]),
+        ]),
+      ]),
+      // transition(':leave', [
+      //   style({ height: '*' }),
+      //   group([
+      //     animate('500ms 500ms', style({ height: '0'})),
+      //     query(':leave', [
+      //       style({ opacity: 1, transform: 'scale(1)'}),
+      //       animate(1500, style({ opacity: 0, transform: 'scale(0)' }))
+      //     ]),
+      //     query('.btn', [
+      //       style({ transform: 'translateX(0)'}),
+      //       animate('.7s ease-out', style({ transform: 'translateX(-100%)' }))
+      //     ]),
+      //   ]),
+      // ]),
+    ]),
+    // trigger('animateMe', [
+    //   transition('* <=> *', animate('500ms cubic-bezier(.68,-0.73,.26,1.65)', keyframes([
+    //     style({ backgroundColor: "transparent", color: '*', offset: 0 }),
+    //     style({ backgroundColor: "blue", color: 'white', offset: 0.2 }),
+    //     style({ backgroundColor: "transparent", color: '*', offset: 1 })
+    //   ])))
+    // ]),
+  ]
 })
-export class MainComponent implements OnInit {
+export class MainComponent implements OnInit, AfterContentChecked {
 
   constructor() { }
+  show = true;
 
   ngOnInit(): void {
+    // setInterval(()=>this.show = !this.show, 8000)
   }
 
   // Carousel
@@ -23,18 +74,22 @@ export class MainComponent implements OnInit {
     pullDrag: false,
     dots: true,
     navSpeed: 2000,
-    navText: ['<', '>',],
+    navText: ['', '',],
     responsive: {
       1024: {
         items: 1
       }
     },
-    nav: true
+    nav: false
   }
     slides = [
-      {id: '1', img: "assets/slides/slide-01.jpg"},
-      {id: '2', img: "assets/slides/slide-02.jpg"},
-      {id: '3', img: "assets/slides/slide-03.jpg"},
-      {id: '4', img: "assets/slides/slide-04.jpg"},
+      {id: '1', img: "assets/slides/slide-01.jpg", title: "Women Collection 2018", desc: "NEW SEASON", btn: "Shop Now"},
+      {id: '2', img: "assets/slides/slide-02.jpg", title: "Men New-Season", desc: "Jackets & Coats", btn: "Shop Now"},
+      {id: '3', img: "assets/slides/slide-03.jpg", title: "Men Collection 2018", desc: "New arrivals", btn: "Shop Now"},
+      {id: '4', img: "assets/slides/slide-04.jpg", title: "Women Collection 2018", desc: "New arrivals", btn: "Shop Now"},
     ];
+
+    ngAfterContentChecked(): void {
+      this.show = !this.show
+    }
 }
