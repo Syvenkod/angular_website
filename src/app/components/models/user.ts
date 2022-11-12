c174d3fe9808cf7610d92fb7a83ca82449eebba2
Dmytro Syvenko <syvenkod@gmail.com>
2022-11-12 22:18:07 +0200

    style category sort

--- src/app/components/models/user.ts
+++ src/app/components/models/user.ts
@@ -1,16 +1,15 @@
 export class User{
-    readonly id: number;
-    readonly email: any;
-    readonly username: any;
-    readonly password: any;
-    readonly name:{
-        firstname:string;
-        lastname:string;
-    }
-    readonly address:{};
-    readonly phone: number;
-
-     constructor (id:number){
+     constructor (readonly id: number,
+        readonly email: any,
+        readonly username: any,
+        readonly password: any,
+        readonly name:{
+            firstname:string,
+            lastname:string,
+        },
+        readonly address:{},
+        readonly phone: number)
+        {
          this.id = id;
          }
    }
